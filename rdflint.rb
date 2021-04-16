d1a96b32be7aba43648bc5e08fc8fafe1b34dba7
Mikami, Takeshi <takeshi.mikami@gmail.com>
2021-04-16 23:36:28 +0900

    add brew style check

--- rdflint.rb
+++ rdflint.rb
@@ -1,6 +1,11 @@
-RDFLINT_VERSION='0.1.5'
+# typed: false
+# frozen_string_literal: true
+
+RDFLINT_VERSION="0.1.5"
+
+# Rdflint Formula
 class Rdflint < Formula
-  desc "rdflint: RDF linter"
+  desc ": RDF linter"
   homepage "https://github.com/imas/rdflint"
   url "https://jitpack.io/com/github/imas/rdflint/#{RDFLINT_VERSION}/rdflint-#{RDFLINT_VERSION}.jar"
   sha256 "d96520dcc355a47cb71449d89ebebf460a1eb9d3bb3a379efbb65724972386c7"
